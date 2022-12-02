4b9f8268c5ca5568c521f181311ada0ae58932b6
mauriciobelusso <mauriciobelusso@gmail.com>
2022-12-01 23:27:19 -0300

    Implementado relatório de vazamentos de memória quando depurando

--- Checkout.dpr
+++ Checkout.dpr
@@ -2,12 +2,16 @@ program Checkout;
 
 uses
   Vcl.Forms,
-  Main in 'Main.pas' {FrmMain};
+  Main in 'Main.pas',
+  System.Classes {FrmMain};
 
 {$R *.res}
 
 begin
   Application.Initialize;
+
+  ReportMemoryLeaksOnShutdown := DebugHook <> 0;
+
   Application.MainFormOnTaskbar := True;
   Application.CreateForm(TFrmMain, FrmMain);
   Application.Run;

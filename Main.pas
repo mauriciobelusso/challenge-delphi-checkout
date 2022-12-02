39126f3fee61610e36e8d81cbb420b8e7df83d30
mauriciobelusso <mauriciobelusso@gmail.com>
2022-12-02 15:56:17 -0300

    Implementando camada da view para o model

--- Main.pas
+++ Main.pas
@@ -9,7 +9,7 @@ uses
   checkout.controller.orders.ordersinterfaces, FireDAC.Stan.Intf,
   FireDAC.Stan.Option, FireDAC.Stan.Param, FireDAC.Stan.Error, FireDAC.DatS,
   FireDAC.Phys.Intf, FireDAC.DApt.Intf, FireDAC.Comp.DataSet,
-  FireDAC.Comp.Client;
+  FireDAC.Comp.Client, checkout.model.entity.orders, checkout.model.entity.orders_products;
 
 type
   TFrmMain = class(TForm)
@@ -33,6 +33,9 @@ type
   private
     { Private declarations }
     FController: iControllerOrders;
+    procedure BindModelToView(const AOrder: TORDERS);
+    procedure BindViewToModel(const AOrder: TORDERS);
+    procedure OpenOrderProducts;
   public
     { Public declarations }
   end;
@@ -44,9 +47,58 @@ implementation
 
 {$R *.dfm}
 
-procedure TFrmMain.btnFinalizarVendaClick(Sender: TObject);
+procedure TFrmMain.BindModelToView(const AOrder: TORDERS);
 begin
-  FController.Save(nil);
+
+end;
+
+procedure TFrmMain.BindViewToModel(const AOrder: TORDERS);
+var
+  LBookmark: TBookmark;
+  LProduct: TORDERS_PRODUCTS;
+begin
+  AOrder.CUSTOMER_ID := 0;
+  AOrder.ISSUE_DATE := Now;
+  AOrder.TOTAL := 0;
+
+  try
+    FDMemTable1.DisableControls;
+    FDMemTable1.Bookmark;
+    FDMemTable1.First;
+    while not FDMemTable1.Eof do
+    begin
+      LProduct := TORDERS_PRODUCTS.Create;
+      try
+        LProduct.ID := FDMemTable1.FieldByName('ID').AsInteger;
+        LProduct.ORDER_ID := FDMemTable1.FieldByName('ORDER_ID').AsInteger;
+        LProduct.QUANTITY := FDMemTable1.FieldByName('QUANTITY').AsFloat;
+        LProduct.UNIT_VALUE := FDMemTable1.FieldByName('UNIT_VALUE').AsCurrency;
+        LProduct.TOTAL := FDMemTable1.FieldByName('TOTAL').AsCurrency;
+      except
+        LProduct.Free;
+        raise;
+      end;
+      AOrder.AddProduct(LProduct);
+      FDMemTable1.Next;
+    end;
+  finally
+    FDMemTable1.EnableControls;
+    FDMemTable1.Bookmark := LBookmark;
+  end;
+end;
+
+procedure TFrmMain.btnFinalizarVendaClick(Sender: TObject);
+var
+  LOrder: TORDERS;
+begin
+  LOrder := TORDERS.Create;
+  try
+    BindViewToModel(LOrder);
+    FController.Save(LOrder);
+    OpenOrderProducts;
+  finally
+    LOrder.Free;
+  end;
 end;
 
 procedure TFrmMain.FormCreate(Sender: TObject);
@@ -57,12 +109,17 @@ begin
 end;
 
 procedure TFrmMain.FormShow(Sender: TObject);
+begin
+  OpenOrderProducts;
+end;
+
+procedure TFrmMain.OpenOrderProducts;
 var
   LQuery: TFDQuery;
 begin
   LQuery := nil;
   try
-    FController.Items.FindByOrderId(0, TDataSet(LQuery));
+    FController.Items.FindByOrderId(-1, TDataSet(LQuery));
     FDMemTable1.CloneCursor(LQuery);
   finally
     LQuery.Free;

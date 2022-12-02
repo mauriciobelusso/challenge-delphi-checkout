93e215deb19982902caf586626a77e6f1acbd180
mauriciobelusso <mauriciobelusso@gmail.com>
2022-12-02 00:26:06 -0300

    Adicionada diretiva para remover warning da DebugHook referente a plataforma

--- Checkout.dpr
+++ Checkout.dpr
@@ -7,14 +7,18 @@ uses
   checkout.model.entity.orders in 'Model\Entity\checkout.model.entity.orders.pas',
   checkout.model.entity.customers in 'Model\Entity\checkout.model.entity.customers.pas',
   checkout.model.entity.products in 'Model\Entity\checkout.model.entity.products.pas',
-  checkout.model.entity.orders_products in 'Model\Entity\checkout.model.entity.orders_products.pas';
+  checkout.model.entity.orders_products in 'Model\Entity\checkout.model.entity.orders_products.pas',
+  checkout.model.orders in 'Model\Orders\checkout.model.orders.pas',
+  checkout.model.orders.interfaces in 'Model\Orders\checkout.model.orders.interfaces.pas';
 
 {$R *.res}
 
 begin
   Application.Initialize;
 
+  {$WARNINGS OFF}
   ReportMemoryLeaksOnShutdown := DebugHook <> 0;
+  {$WARNINGS ON}
 
   Application.MainFormOnTaskbar := True;
   Application.CreateForm(TFrmMain, FrmMain);
